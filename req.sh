6de2b64a2fe9b5b129e9399bc22a165b59f7d6df
FREEDOMTOKEN <39937966+freedomcoin1@users.noreply.github.com>
2018-08-09 17:39:21 -0700

    REBUILD

--- req.sh
+++
@@ -1,2 +0,0 @@
-sudo apt update && sudo apt install build-essential cmake pkg-config libboost-all-dev libssl-dev libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev doxygen graphviz libpcsclite-dev;
-sudo apt-get install libgtest-dev && cd /usr/src/gtest && sudo cmake . && sudo make && sudo mv libg* /usr/lib/; git submodule init && git submodule update; rm -rf /root/.bitFreeDomCrypto/
--- src/version.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-extern const char* const FreeDomCrypto_VERSION_TAG;
-extern const char* const FreeDomCrypto_VERSION;
-extern const char* const FreeDomCrypto_RELEASE_NAME;
-extern const char* const FreeDomCrypto_VERSION_FULL;
